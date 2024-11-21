546b8c066ba13f7f7b820c54b53c9bb7be892285
AwayH <86350556+AwayH@users.noreply.github.com>
2024-11-21 21:56:10 +0800

    add js

---
+++ js/master.js
@@ -0,0 +1,13 @@
+console.log($(window).width());
+
+// let isMobile;
+
+// if($(window).width() > 480) {
+//     isMobile = false;
+// }else {
+//     isMobile = true;
+// }
+
+const isMobile = $(window).width() <= 480
+
+console.log(isMobile);
