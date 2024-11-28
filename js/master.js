2d0680f5c28e1958526460409727f17fbb620c19
AwayH <86350556+AwayH@users.noreply.github.com>
2024-11-28 20:50:34 +0800

    完成

--- js/master.js
+++ js/master.js
@@ -1,4 +1,4 @@
-console.log($(window).width());
+// console.log($(window).width());
 
 // let isMobile;
 
@@ -8,6 +8,49 @@ console.log($(window).width());
 //     isMobile = true;
 // }
 
-const isMobile = $(window).width() <= 480
+// console.log('1 == 1: ', 1 == 1);
+// console.log('1 === 1: ', 1 === 1);
+// console.log('1 == "1": ', 1 == 1);
+// console.log('1 === "1": ', 1 === '1');
+// console.log('1 == true: ', 1 == true);
+// console.log('1 === true: ', 1 === true);
 
-console.log(isMobile);
+const isMobile = $(window).width() <= 480;
+const $grid = $('#Grid');
+
+// console.log(isMobile);
+// console.log($nav);
+
+function setGrid() {
+  if (isMobile) {
+    $grid.addClass('grid-2');
+    return;
+  }
+  $grid.addClass('grid-3');
+}
+
+function setScroll() {
+  if (!isMobile) return;
+
+  const $nav = $('#Nav');
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $nav.addClass('nav-active');
+      return;
+    }
+    $nav.removeClass('nav-active')
+  });
+
+}
+
+function setInit() {
+  setGrid();
+}
+
+function setEvent() {
+  setScroll();
+}
+
+setInit();
+setEvent();
