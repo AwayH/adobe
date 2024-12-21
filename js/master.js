ca00e448c25fe4e38bef6dee19b03ecfbd4240c9
AwayH <86350556+AwayH@users.noreply.github.com>
2024-12-21 20:18:16 +0800

    完成

--- js/master.js
+++ js/master.js
@@ -1,4 +1,83 @@
-let isMobile;
+// let isMobile;
 
-// console.log($(window).innerWidth());
-// console.log($(window).width());
+// if ($(window).width() <= 480) {
+//     isMobile = true;
+// } else {
+//     isMobile = false;
+// }
+const isMobile = $(window).width() <= 480;
+const $grid = $('#Grid');
+
+function setGrid() {
+    if (!isMobile) {
+        $grid.addClass('grid-3');
+        return;
+    }
+    $grid.addClass('grid-2');
+}
+
+function setScroll() {
+    if (!isMobile) return;
+    const $nav = $('#Nav');
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() === 0) {
+            $nav.removeClass('nav-active');
+        } else {
+            $nav.addClass('nav-active');
+        }
+    });
+}
+
+function setFancybox() {
+    const $anchors = $grid.find('a');
+
+    $anchors.fancybox({
+        protect: true,
+        loop: true,
+        // animationDuration: 1000,
+        // transitionDuration: 3000,
+        transitionEffect: 'rotate',
+        keyboard: false,
+        // fullScreen: {
+        //     autoStart: true,
+        // }
+        // infobar: false,
+        // toolbar: false,
+        // smallBtn: false,
+        // arrows: false,
+        // slideShow: {
+        //     autoStart: true,
+        //     speed: 10000,
+        // }
+        // thumbs: false,
+        buttons: ['share', 'download'],
+        media: {
+            youtube: {
+                params: {
+                    autoplay: false,
+                }
+            }
+        }
+    });
+}
+
+function init() {
+    setGrid();
+}
+
+function setEvent() {
+    setScroll();
+    setFancybox();
+}
+
+init();
+setEvent();
+
+
+
+
+
+
+
